d92752e17c1041240d1f9e07525a5d50dd85ad8c
Gareth Hay <gazhay@gmail.com>
2017-07-03 15:36:45 +0100

    test

--- Drop.py
+++ Drop.py
@@ -162,7 +162,7 @@ class TransferHandler(BaseHTTPRequestHandler):
                 # self.path = '/'+servername+'/'+self.path
                 # servern name is .lan
                 print("["+MYHOSTNAME+"]"+"[OVERRIDE] sending to "+DropRoot+servername+"/"+self.path)
-                inf = open(DropRoot+self.path)
+                inf = open(DropRoot+self.path[1:])
                 self.wfile.write(inf.read())
                 inf.close()
                 # return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
